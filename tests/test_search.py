c2ec371bc393509aad830960f07168b8f2a0729c
Miewliie <miewliie@gmail.com>
2023-01-10 15:11:04 -0800

    add test steps and assertion from the test

--- tests/test_search.py
+++ tests/test_search.py
@@ -8,21 +8,30 @@ command in the test, once done it will return to conftest.py to quit.
 
 """
 
+
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
+
+
 def test_basicduckduckgo_search(browser):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+    PHRASE = "duck"
 
     # Given the DuckDuckGo home page is displayed
-    # TODO
+    search_page.load()
 
     # When the user searches for "duck"
-    # TODO
+    search_page.search(PHRASE)
 
     # Then the search result title contains "duck"
-    # TODO
+    assert PHRASE in result_page.title()
 
     # And the search result query is "duck"
-    # TODO
+    assert PHRASE == result_page.search_input_value()
 
     # And the search result links pertain to "duck"
-    # TODO
+    for title in result_page.result_link_titles():
+        assert PHRASE.lower() in title.lower()
 
     raise Exception("Incomplete Test")
