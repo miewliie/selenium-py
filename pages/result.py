cf46afac2f0b3dcdd566a5918e3a1a5f399c95a1
Miewliie <miewliie@gmail.com>
2023-01-10 15:48:43 -0800

    Add search form locator by ID and update gitignore

--- pages/result.py
+++ pages/result.py
@@ -3,8 +3,11 @@ This module contains DuckDuckGoResultPage,
 the page object for the DuckDuckGo search result page.
 """
 
+from selenium.webdriver.common.by import By 
 
 class DuckDuckGoResultPage:
+
+    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
 
     def __init__(self, browser):
         self.browser = browser
