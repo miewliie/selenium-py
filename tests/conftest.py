9f7f1d2cb682ad46b8f984bf4cd66215eade6618
Miewliie <miewliie@gmail.com>
2023-01-09 16:44:41 -0800

    Install selenium and setup fixture  file

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import selenium.webdriver
+
+
+@pytest.fixture
+def browser():
+
+    # Initialize the ChromeDriver instance
+    b = selenium.webdriver.Chrome()
+
+    # Make its calls wait up to 12 seconds for elements to appear
+    b.implicitly_wait(12)
+
+    # Return the WebDriver instance for the setup
+    yield b
+
+    # Quit the WebDriver instance for the cleanup
+    b.quit()
