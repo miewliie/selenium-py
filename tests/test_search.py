681db832da338992140660e56aab98fca2dfd787
Miewliie <miewliie@gmail.com>
2023-01-09 16:51:43 -0800

    Plan test steps by using Given,When,Then

---
+++ tests/test_search.py
@@ -0,0 +1,21 @@
+""" Test DuckDuckGo Searches """
+
+# `test` always start name with test as python will look for it.
+def test_basicduckduckgo_search(browser):
+
+    # Given the DuckDuckGo home page is displayed
+    # TODO
+
+    # When the user searches for "duck"
+    # TODO
+
+    # Then the search result title contains "duck"
+    # TODO
+
+    # And the search result query is "duck"
+    # TODO
+
+    # And the search result links pertain to "duck"
+    # TODO
+
+    raise Exception("Incomplete Test")
