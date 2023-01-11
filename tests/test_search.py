e3c42c953710a5d76225c7215e7f31703e7f13f6
Miewliie <miewliie@gmail.com>
2023-01-10 22:27:52 -0800

    Support multi browser and search in many words

--- tests/test_search.py
+++ tests/test_search.py
@@ -8,30 +8,30 @@ command in the test, once done it will return to conftest.py to quit.
 
 """
 
-
+import pytest
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
 
-def test_basicduckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['cat', 'duck', 'python language'])
+def test_basicduckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "duck"
 
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
-    # When the user searches for "duck"
-    search_page.search(PHRASE)
+    # When the user searches for phrase
+    search_page.search(phrase)
 
-    # Then the search result title contains "duck"
-    assert PHRASE in result_page.title()
+    # Then the search result query is phrase
+    assert phrase == result_page.search_input_value()
 
-    # And the search result query is "duck"
-    assert PHRASE == result_page.search_input_value()
+    # And the search result links pertain to phrase
+    titles = result_page.result_link_titles()
+    matches = [t for t in titles if phrase.lower() in t.lower()]
+    assert len(matches) > 0
 
-    # And the search result links pertain to "duck"
-    for title in result_page.result_link_titles():
-        assert PHRASE.lower() in title.lower()
+    # And the search result title contains phrase
+    assert phrase in result_page.title()
 
-    raise Exception("Incomplete Test")
